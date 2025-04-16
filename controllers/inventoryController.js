2e18eedd6f012054a35cebde2188999d42442284
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-04-16 16:05:42 +0200

    feat: add express-async-handler functionality for error handling

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,92 +1,70 @@
+const asyncHandler = require('express-async-handler');
 const db = require('../db/queries');
+// const { body, validationResult } = require('express-validator');
 
-const manufacturers = [
-  { 
-		id: 1, 
-		name: 'Toyota', 
-		founded: 1937, 
-		owner: 'Toyota Group'
-	},
-  { 
-		id: 2, 
-		name: 'Nissan', 
-		founded: 1933, 
-		owner: 'Renault' 
-	},
-];
+// const validateManufacturer = [
 
-const cars = [
-  { 
-		id: 1, 
-		model: 'GT-R R34', 
-		price: 120990, 
-		year: 2001, 
-		mileage: 52226, 
-		engine: '2.6L Twin-Turbo I6 (RB26DETT)', 
-		hp: 280,
-		manufacturer_id: 2,
-	},
-  { 
-		id: 2, 
-		model: 'Supra', 
-		price: 72250, 
-		year: 1996, 
-		mileage: 76344, 
-		engine: '3.0L Twin-Turbo I6 (2JZ-GTE)*', 
-		hp: 320,
-		manufacturer_id: 1,
-	},
-];
+// ];
 
-async function getManufacturers(req, res) {
+// const validateCar = [
+
+// ];
+
+const getManufacturers = asyncHandler(async (req, res) => {
 	const manufacturers = await db.getManufacturers();
-	
-	res.render('index', {
-		manufacturers: manufacturers
-	});
-};
+	res.render('index', { manufacturers });
+});
 
-async function getManufacturerCars(req, res) {
-	const manufacturer = await db.getManufacturer(req.params.manufacturer);
+const getManufacturerCars = asyncHandler(async (req, res) => {
+	const manufacturer = await db.getManufacturerByName(req.params.manufacturer);
+
+	if (!manufacturer) {
+		res.status(404).send('Manufacturer not found');
+		return;
+	}
+
 	const cars = await db.getManufacturerCars(manufacturer.name);
 
 	res.render('manufacturer', {
-		manufacturer: manufacturer,
-		cars: cars
+		manufacturer,
+		cars
 	});
-};
+});
 
-async function getCar(req, res) {
-	const car = await db.getCar(req.params.carId);
+const getCar = asyncHandler(async (req, res) => {
+	const car = await db.getCarById(req.params.carId);
 
-	res.render('car', {
-		car: car
-	});
-};
+	if (!car) {
+		res.status(404).send('Car not found');
+		return;
+	}
 
-async function createManufacturerGet(req, res) {
+	res.render('car', { car });
+});
+
+const createManufacturerGet = (req, res) => {
 	res.render('createManufacturer');
 };
 
-async function createManufacturerPost(req, res) {
+const createManufacturerPost = asyncHandler(async (req, res) => {
 	// receive form data from req.body
 	// validate/sanitize
 	// insert into db
 	// redirect to /
 	res.redirect('/');
-};
+});
 
-async function createCarGet(req, res) {
+const createCarGet = (req, res) => {
 	res.render('createCar');
 };
 
-async function createCarPost(req, res) {
+const createCarPost = asyncHandler(async (req, res) => {
 	// receive form data from req.body
 	// manufacturer_id will come from select input value
 	// validate/sanitize
 	// insert into db
 	// redirect to /:manufacturer where the car was created
-};
+});
 
 module.exports = {
 	getManufacturers,
