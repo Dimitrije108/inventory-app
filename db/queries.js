cf06dd26577286f7aab13ae4cfc4356aaa62ca6d
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-04-16 13:33:28 +0200

    feat: implement get and insert queries for manufacturers and cars
    Queries to retrieve all manufacturers, a manufacturer, manufacturer cars, a car, and to insert a new manufacturer and car into db.

--- db/queries.js
+++ db/queries.js
@@ -1,21 +1,70 @@
 const pool = require('./pool');
-// Setup examples
-async function getAllMessages() {
-	const { rows } = await pool.query('SELECT * FROM messages');
+
+async function getManufacturers() {
+	const { rows } = await pool.query('SELECT * FROM manufacturers');
 	return rows || null;
-}
+};
 
-async function getMessage(id) {
-	const { rows } = await pool.query('SELECT * FROM messages WHERE id = ($1)', [id]);
-	return rows[0] || null;
-}
+async function getManufacturer(manufacturer) {
+	const { rows } = await pool.query(`
+		SELECT * FROM manufacturers
+		WHERE manufacturers.name = ($1)
+		`, [manufacturer]
+	);
+	return rows || null;
+};
+// Get all cars from a specific manufacturer
+async function getManufacturerCars(manufacturer) {
+	const { rows } = await pool.query(`
+		SELECT 
+			cars.*,
+			manufacturers.name AS manufacturer_name
+		FROM cars 
+		JOIN manufacturers ON cars.manufacturer_id = manufacturers.id 
+		WHERE manufacturers.name = ($1)
+		`, [manufacturer]
+	);
+	return rows || null;
+};
 
-async function insertMessage(msg) {
-	await pool.query('INSERT INTO messages VALUES (DEFAULT, $1, $2, $3)', [...msg]); 
-}
+async function getCar(carId) {
+	const { rows } = await pool.query(`
+		SELECT 
+			cars.*,
+			manufacturers.name AS manufacturer_name
+		FROM cars 
+		JOIN manufacturers ON cars.manufacturer_id = manufacturers.id
+		WHERE cars.id = ($1)
+		`, [carId]
+	);
+	return rows || null;
+};
+
+async function insertManufacturer(data) {
+	const { rows } = await pool.query(`
+		INSERT INTO manufacturers
+		VALUES
+			(DEFAULT, $1, $2, $3)
+		`, [...data]
+	);
+	return rows || null;
+};
+
+async function insertCar(data) {
+	const { rows } = await pool.query(`
+		INSERT INTO cars
+		VALUES
+			(DEFAULT, $1, $2, $3, $4, $5, $6, $7)
+		`, [...data]
+	);
+	return rows || null;
+};
 
 module.exports = {
-	getAllMessages,
-	getMessage,
-	insertMessage,
-}
+	getManufacturers,
+	getManufacturer,
+	getManufacturerCars,
+	getCar,
+	insertManufacturer,
+	insertCar,
+};
