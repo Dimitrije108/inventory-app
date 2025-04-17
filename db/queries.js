28f43647a0e5ecbc0ad2ec4416cf09869b698b18
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-04-17 18:44:23 +0200

    feat: add getManufacturerById query and fix minor syntax issues

--- db/queries.js
+++ db/queries.js
@@ -2,16 +2,25 @@ const pool = require('./pool');
 
 async function getManufacturers() {
 	const { rows } = await pool.query('SELECT * FROM manufacturers');
-	return rows || null;
+	return rows;
 };
 
-async function getManufacturer(manufacturer) {
+async function getManufacturerByName(manufacturer) {
 	const { rows } = await pool.query(`
 		SELECT * FROM manufacturers
 		WHERE manufacturers.name = ($1)
 		`, [manufacturer]
 	);
-	return rows || null;
+	return rows[0] || null;
+};
+
+async function getManufacturerById(manufacturerId) {
+	const { rows } = await pool.query(`
+		SELECT * FROM manufacturers
+		WHERE manufacturers.id = ($1)
+		`, [manufacturerId]
+	);
+	return rows[0] || null;
 };
 // Get all cars from a specific manufacturer
 async function getManufacturerCars(manufacturer) {
@@ -24,10 +33,10 @@ async function getManufacturerCars(manufacturer) {
 		WHERE manufacturers.name = ($1)
 		`, [manufacturer]
 	);
-	return rows || null;
+	return rows;
 };
 
-async function getCar(carId) {
+async function getCarById(carId) {
 	const { rows } = await pool.query(`
 		SELECT 
 			cars.*,
@@ -37,34 +46,33 @@ async function getCar(carId) {
 		WHERE cars.id = ($1)
 		`, [carId]
 	);
-	return rows || null;
+	return rows[0] || null;
 };
 
 async function insertManufacturer(data) {
-	const { rows } = await pool.query(`
+	await pool.query(`
 		INSERT INTO manufacturers
 		VALUES
 			(DEFAULT, $1, $2, $3)
 		`, [...data]
 	);
-	return rows || null;
 };
 
 async function insertCar(data) {
-	const { rows } = await pool.query(`
+	await pool.query(`
 		INSERT INTO cars
 		VALUES
 			(DEFAULT, $1, $2, $3, $4, $5, $6, $7)
 		`, [...data]
 	);
-	return rows || null;
 };
 
 module.exports = {
 	getManufacturers,
-	getManufacturer,
+	getManufacturerByName,
+	getManufacturerById,
 	getManufacturerCars,
-	getCar,
+	getCarById,
 	insertManufacturer,
 	insertCar,
 };
