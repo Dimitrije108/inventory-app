7538d78e16b4c01524662756b6a54aec3f1f366a
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-04-17 18:47:06 +0200

    feat: implement express validation for post forms in the controller

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,14 +1,54 @@
 const asyncHandler = require('express-async-handler');
 const db = require('../db/queries');
-// const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-// const validateManufacturer = [
+const lengthErr = 'must be between 1 and 30 characters.';
+const numberErr = 'must be higher than 0.';
+const yearErr = 'must be a number between 1900 and 2100.';
 
-// ];
+const validateManufacturer = [
+	body('name').trim()
+		.isLength({ min: 1, max: 30 }).withMessage(`Manufacturer name ${lengthErr}`)
+		.custom(async (value) => {
+			const manufacturer = await db.getManufacturerByName(value);
+			if (manufacturer) {
+				throw new Error('Manufacturer name already exists.');
+			}
+			return true;
+		}),
+	body('founded')
+		.isInt({ min: 1900, max: 2100 }).withMessage(`Year ${yearErr}`),
+	body('owner').trim()
+		.optional({ values: 'falsy' })
+		.isLength({ min: 1, max: 30 }).withMessage(`Owner name ${lengthErr}`),
+];
 
-// const validateCar = [
-
-// ];
+const validateCar = [
+	body('manufacturerId')
+		.notEmpty().withMessage('Manufacturer is required.')
+		.custom(async (value) => {
+			const manufacturer = await db.getManufacturerById(value);
+			if (!manufacturer) {
+				throw new Error('Selected manufacturer does not exist.');
+			}
+			return true;
+		}),
+	body('model').trim()
+		.isLength({ min: 1, max: 30 }).withMessage(`Model name ${lengthErr}`),
+	body('price')
+		.isFloat({ min: 1 }).withMessage(`Price ${numberErr}`),
+	body('year')
+		.isInt({ min: 1900, max: 2100 }).withMessage(`Year ${yearErr}`),
+	body('mileage')
+		.optional({ values: 'falsy' })
+		.isFloat({ min: 1 }).withMessage(`Mileage ${numberErr}`),
+	body('engine').trim()
+		.optional({ values: 'falsy' })
+		.isLength({ min: 1, max: 30 }).withMessage(`Engine spec ${lengthErr}`),
+	body('hp')
+		.optional({ values: 'falsy' })
+		.isInt({ min: 1 }).withMessage(`Horsepower ${numberErr}`),
+];
 
 const getManufacturers = asyncHandler(async (req, res) => {
 	const manufacturers = await db.getManufacturers();
@@ -46,25 +86,52 @@ const createManufacturerGet = (req, res) => {
 	res.render('createManufacturer');
 };
 
-const createManufacturerPost = asyncHandler(async (req, res) => {
-	// receive form data from req.body
-	// validate/sanitize
-	// insert into db
-	// redirect to /
-	res.redirect('/');
+const createManufacturerPost = [
+	validateManufacturer,
+	asyncHandler(async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// Return 400 Bad Request because of malformed request
+			return res.status(400).render('createManufacturer', {
+				errors: errors.array()
+			});
+		}
+		
+		const { name, founded, owner }  = req.body;
+		await db.insertManufacturer([name, founded, owner]);
+		res.redirect('/');
+	})
+];
+
+const createCarGet = asyncHandler(async (req, res) => {
+	const manufacturers = await db.getManufacturers();
+	res.render('createCar', {
+		selectedManufacturer: req.params.manufacturer,
+		manufacturers
+	});
 });
 
-const createCarGet = (req, res) => {
-	res.render('createCar');
-};
+const createCarPost = [
+	validateCar,
+	asyncHandler(async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			const manufacturers = await db.getManufacturers();
+			return res.status(400).render('createCar', {
+				selectedManufacturer: req.params.manufacturer,
+				manufacturers,
+				errors: errors.array()
+			})
+		}
 
-const createCarPost = asyncHandler(async (req, res) => {
-	// receive form data from req.body
-	// manufacturer_id will come from select input value
-	// validate/sanitize
-	// insert into db
-	// redirect to /:manufacturer where the car was created
-});
+		const { model, price, year, engine, manufacturerId } = req.body;
+		const mileage = req.body.mileage ? parseInt(req.body.mileage) : null;
+		const hp = req.body.hp ? parseInt(req.body.hp) : null;
+
+		await db.insertCar([model, price, year, mileage, engine, hp, manufacturerId]);
+		res.redirect(`/${req.params.manufacturer}`);
+	})
+] 
 
 module.exports = {
 	getManufacturers,
